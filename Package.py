5acb5162b18f5263d1c71e1175df4c459aef4b6f
cwins <cwinston7g@gmail.com>
2022-05-26 14:21:16 -0400

    edited package class

--- Package.py
+++ Package.py
@@ -15,20 +15,10 @@ class Package:
         self.status = 'at hub'
 
 
-
-        #If I add this attribute, I need to update my package objects that I'm inserting to the hashtable
-
-        #self.delivery_status = delivery_status
-
-# Need to add timestamp attribute which returns time at delivery
-
-
     def __str__(self):
        return "   " + str(self.packageID) + "     " + self.address + ",  " + self.city + ", " +self.state + ", " + str(self.zip) +", " + self.deadline +", " +str(self.weight)  +" Kgs" + ", " + self.notes + ", " +str(self.delivery_time) + ", " + str(self.time_left_hub) + ", " + str(self.status)
 
-    # def __str__(self):
-    #     return "%s, %s, %s, %s, %s, %s, %s, %s"  (
-    #         str(self.packageID), self.address, self.city, self.state, self.deadline,self.weight,str(self.notes),str(self.delivery_time),str(self.time_left_hub))
+
 
     def getPackageID(self):
         return self.packageID
